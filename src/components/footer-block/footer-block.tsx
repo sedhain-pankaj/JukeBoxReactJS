66a205e673941f538f7b641b03cc6f8e81f89441
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2023-04-27 20:51:52 +1000

    padding 10px remove. footer adjusted

--- src/components/footer-block/footer-block.tsx
+++ src/components/footer-block/footer-block.tsx
@@ -1,7 +1,13 @@
+import styles from './footer-block.module.scss';
+
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-footer-blocks-and-templates
  */
 export const FooterBlock = ({}) => {
-    return <span>Craig Williams Promotions. Call 1300-888-581 for Support</span>;
+    return (
+        <span className={styles.footer_text}>
+            Craig Williams Promotions. Call 1300-888-581 for Support
+        </span>
+    );
 };
