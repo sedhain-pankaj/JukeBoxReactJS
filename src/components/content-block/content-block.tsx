380802bcf9527a8ca3e6e116ca51c5d97f8d8537
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2023-04-27 20:06:11 +1000

    arrows html added

--- src/components/content-block/content-block.tsx
+++ src/components/content-block/content-block.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import styles from './content-block.module.scss';
+import 'material-icons/iconfont/outlined.css';
 
 /**
  * This component was created using Codux's Default new component template.
@@ -7,8 +9,33 @@ import styles from './content-block.module.scss';
 export const ContentBlock = ({}) => {
     return (
         <div>
-            <div className={styles.category_block}>Category</div>
-            <div className={styles.arrows_category_block}>arrow</div>
+            <div className={styles.category_block}>
+                <h3>
+                    Click on the categories on the left side. OR <br />
+                    Search songs from top-right corner. AND <br /> <br />
+                    Click on song's THUMBNAIL to queue it.
+                </h3>
+            </div>
+
+            <div className={styles.arrows_category_block}>
+                <button className={styles.arrow_up}>
+                    <i
+                        className="material-icons-outlined"
+                        style={{ fontSize: '3rem', position: 'absolute' }}
+                    >
+                        arrow_circle_up
+                    </i>
+                </button>
+
+                <button className={styles.arrow_down}>
+                    <i
+                        className="material-icons-outlined"
+                        style={{ fontSize: '3rem', position: 'absolute' }}
+                    >
+                        arrow_circle_down
+                    </i>
+                </button>
+            </div>
         </div>
     );
 };
