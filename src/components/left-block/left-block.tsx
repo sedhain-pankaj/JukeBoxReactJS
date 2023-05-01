99d5c58da524ea037ffc0dc146a5fc81d3f01d9a
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2023-05-01 19:17:54 +1000

    volume css: inprogress

--- src/components/left-block/left-block.tsx
+++ src/components/left-block/left-block.tsx
@@ -19,18 +19,20 @@ const category_buttons: string[] = [
     'Christmas Song',
 ];
 
+// let volume_digit: number = 50;
+let volume_digit: number = 50;
+
 export const LeftBlock = ({}) => {
     return (
         <div>
             <div className={styles.category_button}>
                 {/* Shuffle heading and button */}
                 <span className={styles['span-heading']}>Shuffle:</span>
-                <button className={styles['button-randomizer']}>
+                <button className={styles['button-randomizer']} style={{ marginBottom: '0.5rem' }}>
                     <i className="material-icons-outlined">menu</i>
                     &nbsp; &nbsp; Randomizer
                     <i className="material-icons-outlined">sync</i>
                 </button>
-                <br />
 
                 {/* Category heading and buttons */}
                 <span className={styles['span-heading']}>Categories:</span>
@@ -39,16 +41,37 @@ export const LeftBlock = ({}) => {
                         <button className={styles['button-left']}>{button_name}</button>
                     </Fragment>
                 ))}
-                <br />
 
                 {/* YouTube heading and button */}
-                <span className={styles['span-heading']}>YouTube:</span>
+                <span className={styles['span-heading']} style={{ marginTop: '0.5rem' }}>
+                    YouTube:
+                </span>
                 <button className={styles['button-yt']}> YouTube Search</button>
             </div>
 
             {/* Volume heading and buttons */}
             <div className={styles.volume_block}>
-                <span className={styles['span-heading']}>Volume:</span>
+                <span>Volume:</span>
+                <span className={styles['volume-digit']}>{volume_digit}</span>
+
+                <button className={styles['button-mute']}>
+                    Mute:
+                    <i className="material-icons-outlined">volume_up</i>
+                    <i className="material-icons-outlined" style={{ display: 'none' }}>
+                        volume_off
+                    </i>
+                </button>
+                <br />
+
+                <button className={styles['volume-dial']}>
+                    <i className="material-icons-outlined">arrow_circle_left</i>
+                </button>
+
+                <input type="range" min="0" max="100" className={styles['input-slider']} />
+
+                <button className={styles['volume-dial']}>
+                    <i className="material-icons-outlined">arrow_circle_right</i>
+                </button>
             </div>
         </div>
     );
