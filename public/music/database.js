0c184ef12a26a338e2ccaf48a6da1f4cc93fd100
Pankaj Sedhain <kalpanakarki.ps@gmail.com>
2023-05-19 21:23:36 +1000

    json generato working & renamed to database.js

---
+++ public/music/database.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+function createJSON(folderName) {
+    const folderPath = 'music/' + folderName;
+    const MP4_List = fs.readdirSync(folderName).filter((file) => file.endsWith('.mp4'));
+    const songPath = MP4_List.map((file) => folderPath + '/' + file);
+    const songImgPath = MP4_List.map((file) => folderPath + '/img/' + file.replace('.mp4', '.jpg'));
+    const songName = MP4_List.map((file) => file.replace('.mp4', ''));
+    const songSN = MP4_List.map((file) => songName.indexOf(file.replace('.mp4', '')) + 1);
+    const songJson = songSN.map((sn, index) => {
+        return {
+            SN: sn,
+            fullname: songName[index] + '.mp4',
+            shortname: songName[index],
+            path: songPath[index],
+            img: songImgPath[index],
+        };
+    });
+    console.log(songJson);
+    fs.writeFileSync(folderName + '/' + folderName + '.json', JSON.stringify(songJson));
+}
+
+createJSON('Fifty Sixty');
+createJSON('Seventy');
+createJSON('Eighty');
+createJSON('Ninety');
+createJSON('2000');
+createJSON('Latest Hits');
+createJSON('Country');
+createJSON('Karaoke');
+createJSON('Special Occasion');
+createJSON('Christmas Song');
